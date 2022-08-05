f0cb198bfacee0aa17a7f31eedac6f5683bfe2e2
Momin Abbas <41827734+mominabbass@users.noreply.github.com>
2022-08-05 15:37:22 -0400

    Delete maml directory

--- maml/metalearners/__init__.py
+++
@@ -1,4 +0,0 @@
-from maml.metalearners.maml import ModelAgnosticMetaLearning, MAML, FOMAML
-from maml.metalearners.meta_sgd import MetaSGD
-
-__all__ = ['ModelAgnosticMetaLearning', 'MAML', 'FOMAML', 'MetaSGD']
